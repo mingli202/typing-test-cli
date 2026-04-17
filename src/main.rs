11839d61a76045c8503d93041689dc1edf3ba171
mingli202 <vincentmingli@gmail.com>
2026-04-16 20:06:25 -0400

    put args into own module

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,21 @@
 use std::io::{self, Stdout};
 
+use clap::Parser;
 use crossterm::cursor;
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use ratatui::Terminal;
 use ratatui::prelude::CrosstermBackend;
+use typing_test_tui::args::Args;
 use typing_test_tui::run;
-
-use clap::Parser;
-
-// TODO: --offline mode uses my own data
-#[derive(Parser, Debug)]
-#[command(version, about)]
-pub struct Args {
-    /// How many times per second the tui is drawn.
-    #[arg(short, long, default_value_t = 30)]
-    fps: usize,
-
-    /// The tick per second
-    #[arg(short, long, default_value_t = 120)]
-    tps: usize,
-}
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
-    let Args { fps, tps } = Args::parse();
+    let args = Args::parse();
 
     let mut term = setup_terminal()?;
-    run(&mut term, fps, tps).await?;
+    run(&mut term, args).await?;
     teardown_terminal(&mut term)?;
 
     Ok(())
