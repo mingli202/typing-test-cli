03e551133b07735046e91a1178aba752df11be13
mingli202 <vincentmingli@gmail.com>
2026-04-25 10:30:50 -0400

    test(group): new and add user

--- backend/handlers/hub/group/group_test.go
+++ backend/handlers/hub/group/group_test.go
@@ -1,23 +1,50 @@
 package group_test
 
 import (
+	"slices"
 	"testing"
 	"tui/backend/handlers/hub/group"
+	"tui/backend/handlers/hub/user"
 	"tui/backend/services/data_provider"
 )
 
 var dataProvider, _ = data_provider.NewDataProvider()
 
-func TestNewGroup(t *testing.T) {
+func newGroup() *group.Group {
 	data, _ := dataProvider.NewData()
 	group := group.NewGroup("asdf", data)
 
-	users := group.GetUsersSnapshot()
+	return &group
+}
 
-	if users == nil || len(users) != 1 {
+func TestNewGroup(t *testing.T) {
+	gr := newGroup()
+
+	users := gr.GetUsersSnapshot()
+
+	if users == nil {
 		t.Error("group.users should not be nil")
+	}
+
+	if len(users) != 0 {
+		t.Error("There should be no users")
 	}
 }
 
 func TestGroupAddUser(t *testing.T) {
+	u := user.NewUser(nil)
+
+	gr := newGroup()
+
+	gr.AddUser(&u)
+
+	users := gr.GetUsersSnapshot()
+
+	if len(users) != 1 || !slices.Contains(users, &u) {
+		t.Error("It should have added the added user")
+	}
+
+	if *u.GroupId != gr.Id() {
+		t.Error("user group should have been set to the group's id")
+	}
 }
