ae3a16d26cbcc13fde8fdd85d28f31627b79392d
mingli202 <vincentmingli@gmail.com>
2026-05-08 16:21:53 -0400

    feat: wsmsg

--- tui/src/multiplayer/models.rs
+++ tui/src/multiplayer/models.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt::Display;
 
 use serde::{Deserialize, Serialize};
 
@@ -28,4 +29,52 @@ pub struct LobbyInfo {
 pub struct NewGame {
     pub data: Data,
     pub players_info: PlayerInfoSnapshot,
+}
+
+#[derive(Debug)]
+pub enum WsMsg {
+    NewGroup,
+    JoinGroup(String),
+    LeaveGroup,
+    UpdateStats(f64, u8),
+    StartGame,
+}
+
+impl Display for WsMsg {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            WsMsg::JoinGroup(group_id) => format!("JoinGroup {}", group_id),
+            WsMsg::UpdateStats(wpm, progress) => format!("UpdateStats {:.1} {}", wpm, progress),
+            _ => format!("{:?}", self),
+        };
+
+        write!(f, "{}", s)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::WsMsg;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn test_wsmsg_to_string_1() {
+        let msg = WsMsg::NewGroup;
+
+        assert_eq!("NewGroup".to_string(), msg.to_string())
+    }
+
+    #[test]
+    fn test_wsmsg_to_string_2() {
+        let msg = WsMsg::JoinGroup("asdfgh".to_string());
+
+        assert_eq!("JoinGroup asdfgh".to_string(), msg.to_string())
+    }
+
+    #[test]
+    fn test_wsmsg_to_string_3() {
+        let msg = WsMsg::UpdateStats(10.123, 100);
+
+        assert_eq!("UpdateStats 10.1 100".to_string(), msg.to_string())
+    }
 }
