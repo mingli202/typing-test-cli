7cf12d11f3e811b0dfeb5ae8ded328ee65641d09
mingli202 <vincentmingli@gmail.com>
2026-05-19 17:53:05 -0400

    feat(names provider): new name

--- backend/services/name_provider/name_provider.go
+++ backend/services/name_provider/name_provider.go
@@ -2,13 +2,16 @@ package nameprovider
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"math/rand/v2"
 	"tui/backend/assets"
 	"tui/backend/models"
 )
 
 type NameProvider struct {
-	repository models.NamesRepo
+	nouns      []string
+	adjectives []string
 }
 
 // Makes a new name provider
@@ -20,15 +23,36 @@ func NewNameProvider() (NameProvider, error) {
 		return defaultNameProvider(), nil
 	}
 
-	return NameProvider{repository}, nil
+	return NameProvider{
+		nouns:      repository.Nouns,
+		adjectives: repository.Adjectives,
+	}, nil
+}
+
+// Gets a new name
+func (provider *NameProvider) NewName() (string, error) {
+	nounsLen := len(provider.nouns)
+	adLen := len(provider.adjectives)
+
+	if nounsLen == 0 || adLen == 0 {
+		return "Handsome Guest", fmt.Errorf("Oops, no nouns or adjectives")
+	}
+
+	randomNounIndex := rand.IntN(nounsLen)
+	randomAdIndex := rand.IntN(adLen)
+
+	return provider.nouns[randomNounIndex] + provider.adjectives[randomAdIndex], nil
+}
+
+// Returns whether the nouns or adjectives repo is empty or only 1
+func (provider *NameProvider) LessThan2NounsOrAdjectives() bool {
+	return len(provider.nouns) < 2 || len(provider.adjectives) < 2
 }
 
 // Returns a default provider in cases of errors
 func defaultNameProvider() NameProvider {
 	return NameProvider{
-		repository: models.NamesRepo{
-			Names:      []string{"Guest"},
-			Adjectives: []string{"Handsome"},
-		},
+		nouns:      []string{"Guest"},
+		adjectives: []string{"Handsome"},
 	}
 }
